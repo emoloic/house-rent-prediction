612c31cded2371086d2fa2c6da9053643ca7540d
emo <emoloic56@gmail.com>
2022-09-11 17:52:36 +0100

    first commit

--- web-app/web-app.py
+++ web-app/web-app.py
@@ -23,7 +23,8 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.image("./images/image.jpg", width=700)
+image_url = "https://my-esl-bucket.s3.ap-south-1.amazonaws.com/image.jpg"
+st.image(image_url, width=700)
 
 st.markdown(
     "<h4 style='text-align: center; color: blue;'>Predict the rent of your future house in India !!!</h4>",
